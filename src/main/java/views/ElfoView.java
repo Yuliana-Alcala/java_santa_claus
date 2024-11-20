2b8fbc0e6ee1ecccae0628d5361d24bf791985ee
Yuliana-Alcala <alcalabyulianac@gmail.com>
2024-11-20 08:33:58 +0100

    create method selectChild

--- src/main/java/views/ElfoView.java
+++ src/main/java/views/ElfoView.java
@@ -16,7 +16,7 @@ public class ElfoView extends View {
 
             switch (option) {
                 case 1:
-                    System.out.println("1. Añadir juguetes"); 
+                    selectChild(); 
                     break;  
                 case 2:    
                     System.out.println("2. Ver todos los juguetes");  
@@ -38,6 +38,42 @@ public class ElfoView extends View {
       
             
             
+    }
+
+    public static void selectChild(){
+        boolean programRuning = true;
+        while(programRuning){
+            System.out.println("-------------------------"); 
+            System.out.println("Para niño");   
+            System.out.println("1. Bueno");       
+            System.out.println("2. Malo");  
+            System.out.println("Seleccione una opción: ");  
+            int option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    postGoodToy();
+                    break;  
+                case 2:    
+                    System.out.println("2. Añadir juguetes niño malo");  
+                    break;
+                                
+                default:
+                    System.out.println("La opción: "+ option + " es incorrecta");
+                    break;
+            }
+        }
+    }
+
+    public static void postGoodToy() {
+        System.out.println("Añadiendo juguetes para niño bueno");
+        System.out.println("Ingrese el título");
+        String title = scanner.nextLine();
+        System.out.println("Ingrese la marca");
+        String brand = scanner.nextLine();
+        System.out.println("Ingrese la categoría");
+        String category = scanner.nextLine();
+                
     }
 
     public static void closeSession(){
