4e5ea0b87d97e8e9c667be5d34f82fb69811a2aa
Yuliana-Alcala <alcalabyulianac@gmail.com>
2024-11-20 08:10:14 +0100

    implement switch in the views

--- src/main/java/views/ElfoView.java
+++ src/main/java/views/ElfoView.java
@@ -3,17 +3,40 @@ package views;
 public class ElfoView extends View {
 
     public static void index() {
-        System.out.println("-------------------------"); 
-        System.out.println("Gestor de juguetes (Tipo de sesión: Elfo)");   
-        System.out.println("1. Añadir juguetes");       
-        System.out.println("2. Ver todos los juguetes");  
-        System.out.println("3. Eliminar juguetes");       
-        System.out.println("4. Cerrar sesión"); 
-        System.out.println("Seleccione una opción: ");  
-        int option = scanner.nextInt();
+        boolean programRuning = true;
+        while(programRuning){
+            System.out.println("-------------------------"); 
+            System.out.println("Gestor de juguetes (Tipo de sesión: Elfo)");   
+            System.out.println("1. Añadir juguetes");       
+            System.out.println("2. Ver todos los juguetes");  
+            System.out.println("3. Eliminar juguetes");       
+            System.out.println("4. Cerrar sesión"); 
+            System.out.println("Seleccione una opción: ");  
+            int option = scanner.nextInt();
 
-        if (option == 4)
-            closeSession();
+            switch (option) {
+                case 1:
+                    System.out.println("1. Añadir juguetes"); 
+                    break;  
+                case 2:    
+                    System.out.println("2. Ver todos los juguetes");  
+                    break;
+                case 3:
+                    System.out.println("3. Eliminar juguetes");       
+                    break;
+                case 4:
+                    System.out.println("********** Sesión Elfo cerrada *************");
+                    programRuning = false;
+                    closeSession();                 
+                    break;
+                
+                default:
+                    System.out.println("La opción: "+ option + " es incorrecta");
+                    break;
+            }
+        }
+      
+            
             
     }
 
--- src/main/java/views/HomeView.java
+++ src/main/java/views/HomeView.java
@@ -5,25 +5,41 @@ public class HomeView extends View{
 
 
        public static void index() {
-              System.out.println("--------------------------------"); 
-              System.out.println("Iniciar sesión de trabajo como: ");   
-              System.out.println("1. Elfo");       
-              System.out.println("2. Santa");  
-              System.out.println("3. Salir"); 
-              System.out.println("Seleccione una opción: ");  
-              int option = scanner.nextInt();
 
-              if(option == 1)
-                     ElfoView.index();
-              
-              if(option ==3 )
-                     exitProgram();
+              boolean programRuning = true;
+              while(programRuning){
+                     System.out.println("--------------------------------"); 
+                     System.out.println("Iniciar sesión de trabajo como: ");   
+                     System.out.println("1. Elfo");       
+                     System.out.println("2. Santa");  
+                     System.out.println("3. Salir"); 
+                     System.out.println("Seleccione una opción: ");  
+                     int option = scanner.nextInt();
+
+                     
+                     switch (option) {
+                            case 1:
+                                   ElfoView.index();
+                                   break;
+                            case 2:
+                                   System.out.println("2. Santa"); 
+                                   break;
+                            case 3:
+                                   System.out.println("********** Feliz Navidad **********");
+                                   exitProgram();
+                                   break;
+                            default:
+                                   System.out.println("La opción: "+ option + " es incorrecta");
+                                   break;
+                     }
+              }
+           
               
        }
 
        public static void exitProgram(){
-
               scanner.close();
+              System.exit(0);
        }
       
 
